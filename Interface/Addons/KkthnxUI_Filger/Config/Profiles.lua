7128d83471123528a77ec584968019322811f48b
Josh Russell <JRussell20@gmail.com>
2015-10-04 22:59:33 -0400

    Small adjustments.

--- Interface/Addons/KkthnxUI_Filger/Config/Profiles.lua
+++ Interface/Addons/KkthnxUI_Filger/Config/Profiles.lua
@@ -2,13 +2,13 @@
 if C.unitframe.enable ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	ShestakUI_Filger personal configuration file
+--	KkthnxUI_Filger personal configuration file
 --	BACKUP THIS FILE BEFORE UPDATING!
 --	ATTENTION: When saving changes to a file encoded file should be in UTF8
 ----------------------------------------------------------------------------------------
 --	Configuration example:
 ----------------------------------------------------------------------------------------
--- if T.name == "MegaChar" then
+-- if K.Name == "MegaChar" then
 --		C["filger_settings"].show_tooltip = true
 --		C["filger_position"].player_buff_icon = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 173}
 --		add("T_DEBUFF_ICON", {spellID = 115767, unitID = "target", caster = "player", filter = "DEBUFF"})
@@ -20,7 +20,7 @@ if C.unitframe.enable ~= true then return end
 ----------------------------------------------------------------------------------------
 local add = function(place, spell)
 	for class, _ in pairs(C["filger_spells"]) do
-		if class == T.class then
+		if class == K.Class then
 			for i = 1, #C["filger_spells"][class], 1 do
 				if C["filger_spells"][class][i]["Name"] == place then
 					table.insert(C["filger_spells"][class][i], spell)
@@ -30,32 +30,24 @@ local add = function(place, spell)
 	end
 end
 
-----------------------------------------------------------------------------------------
---	Per Class Config (overwrite general)
---	Class need to be UPPERCASE
-----------------------------------------------------------------------------------------
+-- Per Class Config (overwrite general)
+-- Class need to be UPPERCASE
 if K.Class == "DRUID" then
 
 end
 
-----------------------------------------------------------------------------------------
---	Per Character Name Config (overwrite general and class)
---	Name need to be case sensitive
-----------------------------------------------------------------------------------------
+-- Per Character Name Config (overwrite general and class)
+-- Name need to be case sensitive
 if K.Name == "CharacterName" then
 
 end
 
-----------------------------------------------------------------------------------------
---	Per Max Character Level Config (overwrite general, class and name)
-----------------------------------------------------------------------------------------
+-- Per Max Character Level Config (overwrite general, class and name)
 if K.Level ~= MAX_PLAYER_LEVEL then
 
 end
 
-----------------------------------------------------------------------------------------
---	Oz@Pozzo dell'Eternità Config
-----------------------------------------------------------------------------------------
-if K.Name == "Oz" and T.realm == "Pozzo dell'Eternità" then
+-- Kkthnx@Stormreaver KkthnxUI Personal Config
+if K.Name == "Microlite" or "Kkthnx" and K.Realm == "Stormreaver" or "Sargeras" then
 	C["filger_settings"].show_tooltip = true
 end
