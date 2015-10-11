f432b028b33c54dc7ac992e764fe1e761c4fc584
Josh Russell <JRussell20@gmail.com>
2015-10-10 23:18:45 -0400

    Curse stuff

--- KkthnxUI/Config/Settings.lua
+++ KkthnxUI/Config/Settings.lua
@@ -97,6 +97,16 @@ C["actionbar"] = {
 	["skinbuttons"] = true,
 }
 
+-- Nameplate options
+C["nameplate"] = {
+	["enable"] = true,
+	["shortnames"] = true,
+	["showlevel"] = true,
+	["showtotem"] = true,
+	["targetborder"] = true,
+}
+
+
 -- Announcements options
 C["announcements"] = {
 	["badgear"] = true,
